0eefcae309b5920a1d9ac4e882a8d4930ca3f08f
Alan-Yeger <alanyeger1@gmail.com>
2025-06-14 19:54:34 +0000

    Exercise 3 fixed

--- Ej3/examples/main.cpp
+++ Ej3/examples/main.cpp
@@ -16,31 +16,37 @@ struct Tarea
 
 queue<Tarea> colaTareas;
 mutex mtx;
+mutex cout_mtx;
 condition_variable cv;
 bool terminado = false;
 
 int tareas_terminadas = 0;
-const int NUM_TAREAS = 3;
+const int NUM_TAREAS_POR_SENSOR = 3;
+const int NUM_SENSORES = 3;
+const int NUM_ROBOTS = 3;
 
-void crearTarea(int idSensor, int idTarea, string descripcionTarea)
-{
-    Tarea tarea;
-    tarea.idSensor = idSensor;
-    tarea.idTarea = idTarea;
-    tarea.descripcionTarea = descripcionTarea;
+void crearTarea(int idSensor) {
+    for (int i = 1; i <= NUM_TAREAS_POR_SENSOR; ++i) {
+        Tarea tarea;
+        tarea.idSensor = idSensor;
+        tarea.idTarea = i;
+        tarea.descripcionTarea = "Descripcion tarea #" + to_string(i) + " del sensor " + to_string(idSensor);
+    
     {                              // Lo metemos en un scope porque porque el lockguard se libera automaticamente
         lock_guard<mutex> lg(mtx); // se usa el mismo mutex con lockguard y este asegura que solo uno pueda acceder a la vez
         colaTareas.push(tarea);
-        cout << "Tarea creada con id: " << tarea.idTarea << " con descripcion: " << tarea.descripcionTarea << " y con el id del sensor correspondiente a"
-             << tarea.idSensor << endl;
+        cout << "[Sensor " << idSensor << "] Tarea creada: ID_Tarea=" << tarea.idTarea 
+                 << ", Descripción=" << tarea.descripcionTarea << endl;
     }
-    cv.notify_all(); // llamo a los robots
+
     this_thread::sleep_for(chrono::milliseconds(175));
+    cv.notify_all(); // llamo a los robots HAY QUE VER SI DEJAMOS EL NOTIFY_ALL O CAMBIAMOS NOTIFY_ONE COMO DIJO JUANI
+    }
 
     {
         lock_guard<mutex> lg(mtx);
         tareas_terminadas++;
-        if (tareas_terminadas == NUM_TAREAS)
+        if (tareas_terminadas == NUM_SENSORES)
         {
             terminado = true;
             cv.notify_all(); // El último productor notifica que se terminó
@@ -48,61 +54,48 @@ void crearTarea(int idSensor, int idTarea, string descripcionTarea)
     }
 }
 
-void realizarTarea(int id)
-{
-    while (true)
-    {
+void realizarTarea(int id) {
+    while (true) {
         unique_lock<mutex> ul(mtx);
-        cv.wait(ul, []
-                { return !colaTareas.empty() || terminado; });
+        cv.wait(ul, [] { return !colaTareas.empty() || terminado; });
 
-        if (!colaTareas.empty())
-        {
-            Tarea valor = colaTareas.front();
+        if (!colaTareas.empty()) {
+            Tarea tarea = colaTareas.front();
             colaTareas.pop();
-            cout << "[Robot " << id
-                 << "] Procesando la tarea con las siguintes caracteristicas " << "id sensor: " << valor.idSensor << " id tarea: " << valor.idTarea << " descripcion: " << valor.descripcionTarea << endl;
+            cout << "[Robot " << id << "] Procesando tarea: Sensor=" << tarea.idSensor 
+                 << ", ID_Tarea=" << tarea.idTarea 
+                 << ", Descripción=" << tarea.descripcionTarea << endl;
             ul.unlock(); // unlockeamos aca para que ya vaya procesando otra tarea y funcione el multithreading
             this_thread::sleep_for(chrono::milliseconds(250));
         }
-        else if (terminado)
-        {
+        else if (terminado) {
             break;
         }
     }
-    cout << "[Robot con id: " << id << "] terminó su tarea.\n";
+    {
+        lock_guard<mutex> lock(cout_mtx);
+        cout << "[Robot con id: " << id << "] terminó su tarea.\n";
+    }
 }
 
 int main()
 {
-    const int NUM_SENSORES = 3;
-    const int NUM_TAREAS_POR_SENSOR = 3;
-    const int NUM_ROBOTS = 3;
-
-    vector<thread> sensores;
-    // Crear tareas: 3 tareas por sensor → en total 9 tareas generadas
-    for (int idSensor = 1; idSensor <= NUM_SENSORES; ++idSensor)
     {
-        for (int idTarea = 1; idTarea <= NUM_TAREAS_POR_SENSOR; ++idTarea)
-        {
-            string descripcion = "Descripcion tarea #" + to_string(idTarea) + " del sensor " + to_string(idSensor);
-            sensores.emplace_back(crearTarea, idSensor, idTarea, descripcion);
+        vector<jthread> sensores;
+        vector<jthread> robots;
+
+        // Crear sensores
+        for (int i = 1; i <= NUM_SENSORES; ++i) {
+            sensores.emplace_back(crearTarea, i);
+        }
+
+        // Crear robots
+        for (int i = 1; i <= NUM_ROBOTS; ++i) {
+            robots.emplace_back(realizarTarea, i);
         }
     }
-
-    vector<thread> robots;
-    for (int i = 1; i <= NUM_ROBOTS; ++i)
-        robots.emplace_back(realizarTarea, i);
-
-    // Esperar que terminen todos los sensores
-    for (auto &s : sensores)
-        s.join();
-
-    // Esperar que terminen todos los robots
-    for (auto &r : robots)
-        r.join();
-
+    
     cout << ">>> Todos los sensores y robots han terminado." << endl;
-
+    
     return 0;
 }
